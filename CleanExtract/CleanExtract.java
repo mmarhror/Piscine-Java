dcbf59002a5ed277bfec81ab8249eaa1d9832ea1
mmarhror <mohammedmarhror@gmail.com>
2026-08-23 16:05:31 +0100

    Quick push

---
+++ CleanExtract/CleanExtract.java
@@ -0,0 +1,24 @@
+
+import java.util.ArrayList;
+
+public class CleanExtract {
+
+    public static String extract(String s) {
+        ArrayList<String> res = new ArrayList<>();
+
+        for (String word : s.split("\\|")) {
+            int f = word.indexOf('.');
+            int l = word.lastIndexOf('.');
+
+            if (f == -1 && l == -1) {
+                res.add(word.trim());
+            }
+
+            if (f != -1 && l != -1 && f < l) {
+                res.add(word.substring(f + 1, l).trim());
+            }
+        }
+
+        return String.join(" ", res);
+    }
+}
