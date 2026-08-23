599a1e6d96490a9c294ec8e1e40a8f5c3ad9a7f8
mmarhror <mohammedmarhror@gmail.com>
2026-08-23 16:57:33 +0100

    Quick push

---
+++ ComputeArray/ComputeArray.java
@@ -0,0 +1,25 @@
+
+public class ComputeArray {
+
+    public static int[] computeArray(int[] array) {
+        if (array == null) {
+            return null;
+        }
+
+        int[] res = new int[array.length];
+
+        for (int i = 0; i < 10; i++) {
+            int a = array[i];
+            res[i] = switch (a % 3) {
+                case 0 ->
+                    a * 5;
+                case 1 ->
+                    a + 7;
+                default ->
+                    a;
+            };
+        }
+
+        return res;
+    }
+}
