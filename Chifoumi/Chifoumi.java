2a81845beb318c539df6f33980ae623f11388643
mmarhror <mohammedmarhror@gmail.com>
2026-08-23 15:04:47 +0100

    Quick push

--- Chifoumi/Chifoumi.java
+++ Chifoumi/Chifoumi.java
@@ -4,11 +4,11 @@ public class Chifoumi {
     public static ChifoumiAction getActionBeatenBy(ChifoumiAction chifoumiAction) {
         return switch (chifoumiAction) {
             case ROCK ->
-                ChifoumiAction.PAPER;
-            case PAPER ->
                 ChifoumiAction.SCISSOR;
-            default ->
+            case PAPER ->
                 ChifoumiAction.ROCK;
+            default ->
+                ChifoumiAction.PAPER;
         };
     }
 }
