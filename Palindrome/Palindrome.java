3a214dfb9e7120e71e5fad1d1c9fcfe241f5b3a2
mmarhror <mohammedmarhror@gmail.com>
2026-08-23 16:46:03 +0100

    Quick push

--- Palindrome/Palindrome.java
+++ Palindrome/Palindrome.java
@@ -2,6 +2,9 @@
 public class Palindrome {
 
     public static boolean isPalindrome(String s) {
+        if (s == null) {
+            return true;
+        }
         return new StringBuilder(s).reverse().toString().toLowerCase().equals(s.toLowerCase());
     }
 }
