bb38e09f7f1b90deb51e65f68c8f0d9b58543d2a
mmarhror <mohammedmarhror@gmail.com>
2026-08-23 15:15:35 +0100

    Quick push

--- StringReplace/StringReplace.java
+++ StringReplace/StringReplace.java
@@ -2,6 +2,9 @@
 public class StringReplace {
 
     public static String replace(String s, String target, String replacement) {
+        if (target == null || replacement == null) {
+            return s;
+        }
         return s.replace(target, replacement);
     }
 }
