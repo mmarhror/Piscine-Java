bd5ff599931bc4a1656add00ed5e21d4b9074dc4
mmarhror <mohammedmarhror@gmail.com>
2026-08-23 15:10:18 +0100

    Quick push

--- StringConcat/StringConcat.java
+++ StringConcat/StringConcat.java
@@ -2,6 +2,12 @@
 public class StringConcat {
 
     public static String concat(String s1, String s2) {
+        if (s1 == null) {
+            return s2;
+        }
+        if (s2 == null) {
+            return s1;
+        }
         return s1 + s2;
     }
 }
