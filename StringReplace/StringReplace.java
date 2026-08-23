d8e275f5504f1f5508f4120931bb984cad848767
mmarhror <mohammedmarhror@gmail.com>
2026-08-23 15:21:24 +0100

    Quick push

--- StringReplace/StringReplace.java
+++ StringReplace/StringReplace.java
@@ -4,4 +4,8 @@ public class StringReplace {
     public static String replace(String s, char target, char replacement) {
         return s.replace(target, replacement);
     }
+
+    public static String replace(String s, String target, String replacement) {
+        return s.replace(target, replacement);
+    }
 }
