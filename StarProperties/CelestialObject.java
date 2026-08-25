bc7f615884c8234afb68594df2dc0848778eed29
mmarhror <mohammedmarhror@gmail.com>
2026-08-25 12:40:25 +0100

    Quick push

--- StarProperties/CelestialObject.java
+++ StarProperties/CelestialObject.java
@@ -1,8 +1,8 @@
 
 public class CelestialObject {
 
-    double x;
-    double y;
-    double z;
-    String name;
+    public double x;
+    public double y;
+    public double z;
+    public String name;
 }
