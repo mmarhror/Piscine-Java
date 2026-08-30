917aa278b8d213ea85372a0eb1b683bd57396397
mmarhror <mohammedmarhror@gmail.com>
2026-08-30 14:03:05 +0100

    Quick push

---
+++ AdventureWeapon/Weapon.java
@@ -0,0 +1,27 @@
+
+public class Weapon {
+
+    private String name;
+    private int damage;
+
+    public Weapon(String name, int damage) {
+        this.name = name;
+        this.damage = damage;
+    }
+
+    // Getters/Setters
+    public String getName() {
+        return name;
+    }
+
+    public int getDamage() {
+        return damage;
+    }
+
+    // Formatters
+    @Override
+    public String toString() {
+        return String.format("%s deals %d damages", name, damage);
+    }
+
+}
