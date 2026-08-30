3b1d693792d30e8bcb5b18d02507411842610a8a
mmarhror <mohammedmarhror@gmail.com>
2026-08-30 11:43:29 +0100

    Quick push

--- AdventureSorcerer/Sorcerer.java
+++ AdventureSorcerer/Sorcerer.java
@@ -1,7 +1,7 @@
 
 public class Sorcerer extends Character implements Healer {
 
-    private int healCapacity;
+    private final int healCapacity;
 
     public Sorcerer(String name, int maxHealth, int healCapacity) {
         super(name, maxHealth);
