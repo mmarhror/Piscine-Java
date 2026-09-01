b631cbfb88c4c662529ed751d620be63a32b421a
mmarhror <mohammedmarhror@gmail.com>
2026-09-01 11:56:40 +0100

    Quick push

---
+++ ListSearchIndex/ListSearchIndex.java
@@ -0,0 +1,26 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ListSearchIndex {
+
+    public static Integer findLastIndex(List<Integer> list, Integer value) {
+        return list.lastIndexOf(value);
+    }
+
+    public static Integer findFirstIndex(List<Integer> list, Integer value) {
+        return list.indexOf(value);
+    }
+
+    public static List<Integer> findAllIndexes(List<Integer> list, Integer value) {
+        List<Integer> ms = new ArrayList<>();
+
+        for (Integer i : list) {
+            if (i.equals(value)) {
+                ms.add(i);
+            }
+        }
+
+        return ms;
+    }
+}
