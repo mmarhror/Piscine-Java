cd3bc0b878918e60b9b5a332665e3b39afbd465e
mmarhror <mohammedmarhror@gmail.com>
2026-09-01 13:57:02 +0100

    Quick push

--- Quest-05/MapEquals/MapEquals.java
+++ Quest-05/MapEquals/MapEquals.java
@@ -5,7 +5,7 @@ public class MapEquals {
 
     public static boolean areMapsEqual(Map<String, Integer> map1, Map<String, Integer> map2) {
         if (map1 == null) {
-            return false;
+            return map2 == null;
         }
         return map1.equals(map2);
     }
