c8d0d5895fcf29b8d86db31763ab22bcd6132162
mmarhror <mohammedmarhror@gmail.com>
2026-09-01 13:56:50 +0100

    Quick push

--- ListEquals/ListEquals.java
+++ ListEquals/ListEquals.java
@@ -5,7 +5,7 @@ public class ListEquals {
 
     public static boolean areListsEqual(List<String> list1, List<String> list2) {
         if (list1 == null) {
-            return false;
+            return list2 == null;
         }
         return list1.equals(list2);
     }
