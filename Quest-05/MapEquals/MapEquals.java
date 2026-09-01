0a5730260e0cf03e846d7ecfdf5db77108e94d7d
mmarhror <mohammedmarhror@gmail.com>
2026-09-01 13:52:31 +0100

    Quick push

--- Quest-05/MapEquals/MapEquals.java
+++ Quest-05/MapEquals/MapEquals.java
@@ -5,9 +5,6 @@ public class MapEquals {
 
     public static boolean areMapsEqual(Map<String, Integer> map1, Map<String, Integer> map2) {
         if (map1 == null) {
-            if (map2 == null) {
-                return true;
-            }
             return false;
         }
         return map1.equals(map2);
--- SetEquals/SetEquals.java
+++ SetEquals/SetEquals.java
@@ -5,9 +5,6 @@ public class SetEquals {
 
     public static boolean areSetsEqual(Set<String> set1, Set<String> set2) {
         if (set1 == null) {
-            // if (map2 == null) {
-            //     return true;
-            // }
             return false;
         }
         return set1.equals(set2);
