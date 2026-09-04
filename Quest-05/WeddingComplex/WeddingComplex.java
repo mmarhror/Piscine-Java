b20baa5cff6c91e3018085b2c30729a814d096e1
mmarhror <mohammedmarhror@gmail.com>
2026-09-04 16:31:31 +0100

    Quick Push

--- Quest-05/WeddingComplex/WeddingComplex.java
+++ Quest-05/WeddingComplex/WeddingComplex.java
@@ -1,62 +1,56 @@
 
-import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.Map;
+import java.util.Queue;
 
 public class WeddingComplex {
 
-    public static Map<String, String> createBestCouple(
-            Map<String, List<String>> first,
-            Map<String, List<String>> second) {
+    public static Map<String, String> createBestCouple(Map<String, List<String>> first, Map<String, List<String>> second) {
+        Map<String, String> couples = new HashMap<>();
+        Queue<String> single = new LinkedList<>(first.keySet());
 
-        if (first == null || second == null || first.isEmpty() || second.isEmpty()) {
-            return new HashMap<>();
-        }
+        Map<String, Integer> nextFavIdx = new HashMap<>();
 
-        List<String> freeProposers = new ArrayList<>(first.keySet());
+        while (!single.isEmpty()) {
+            String boy = single.poll();
 
-        Map<String, String> engagements = new HashMap<>();
+            int propIdx = nextFavIdx.getOrDefault(boy, 0);
 
-        Map<String, Integer> proposalIndices = new HashMap<>();
-        for (String proposer : first.keySet()) {
-            proposalIndices.put(proposer, 0); 
-        }
+            List<String> favs = first.get(boy);
 
-        while (!freeProposers.isEmpty()) {
-            String proposer = freeProposers.get(0);
-            List<String> proposerPrefs = first.get(proposer);
+            String girl = favs.get(propIdx);
 
-            int nextChoiceIndex = proposalIndices.get(proposer);
-            String acceptor = proposerPrefs.get(nextChoiceIndex);
+            String girlPartner = couples.get(girl);
 
-            proposalIndices.put(proposer, nextChoiceIndex + 1);
+            if (girlPartner == null) {
+                couples.put(girl, boy);
 
-            if (!engagements.containsKey(acceptor)) {
-                engagements.put(acceptor, proposer);
-                freeProposers.remove(proposer); 
-            } 
-            else {
-                String currentPartner = engagements.get(acceptor);
-                List<String> acceptorPrefs = second.get(acceptor);
+            } else {
+                List<String> girlFavs = second.get(girl);
 
-                int preferenceOfNew = acceptorPrefs.indexOf(proposer);
-                int preferenceOfCurrent = acceptorPrefs.indexOf(currentPartner);
+                int boyRank = girlFavs.indexOf(boy) == -1 ? Integer.MAX_VALUE : girlFavs.indexOf(boy);
+                int partnerRank = girlFavs.indexOf(girlPartner) == -1 ? Integer.MAX_VALUE : girlFavs.indexOf(girlPartner);
 
-                if (preferenceOfNew < preferenceOfCurrent) {
-                    engagements.put(acceptor, proposer);
-
-                    freeProposers.remove(proposer);       
-                    freeProposers.add(currentPartner);   
+                if (boyRank < partnerRank) {
+                    couples.put(girl, boy);
+                    single.add(girlPartner);
+                } else {
+                    single.add(boy);
                 }
             }
+
+            nextFavIdx.put(boy, propIdx + 1);
+
         }
 
-        Map<String, String> result = new HashMap<>();
-        for (Map.Entry<String, String> entry : engagements.entrySet()) {
-            result.put(entry.getValue(), entry.getKey());
+        Map<String, String> res = new HashMap<>();
+
+        for (Map.Entry<String, String> ent : couples.entrySet()) {
+            res.put(ent.getValue(), ent.getKey());
         }
 
-        return result;
+        return res;
     }
 }
