655d64850c55d98f927f2c61a7893552bb9fdd1d
mmarhror <mohammedmarhror@gmail.com>
2026-09-07 11:41:54 +0100

    Quick push

--- FormatDate/FormatDate.java
+++ FormatDate/FormatDate.java
@@ -8,18 +8,27 @@ import java.util.Locale;
 public class FormatDate {
 
     public static String formatToFullText(LocalDateTime dateTime) {
+        if (dateTime == null) {
+            return null;
+        }
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("'Le 'dd MMMM' de l''an' yyyy 'à' hh'h'mm'm et 'ss's'", Locale.FRENCH);
 
         return dateTime.format(fmt);
     }
 
     public static String formatSimple(LocalDate date) {
+        if (date == null) {
+            return null;
+        }
         DateTimeFormatter fmt = DateTimeFormatter.ofPattern("MMMM dd yy", Locale.ITALIAN);
 
         return date.format(fmt);
     }
 
     public static String formatIso(LocalTime time) {
+        if (time == null) {
+            return null;
+        }
         return time.toString();
     }
 }
