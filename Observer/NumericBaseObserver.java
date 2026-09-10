41e0777e5e13ea06f2270b98f69986053921fdbd
mmarhror <mohammedmarhror@gmail.com>
2026-09-10 11:54:38 +0100

    Quick push

---
+++ Observer/NumericBaseObserver.java
@@ -0,0 +1,10 @@
+
+import java.util.List;
+
+
+public interface NumericBaseObserver {
+
+    void updateState(int state);
+
+    List<String> getEvents();
+}
---
+++ Observer/ValuePublisher.java
@@ -0,0 +1,22 @@
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class ValuePublisher {
+
+    private List<NumericBaseObserver> observers = new ArrayList<>();
+
+    public void subscribe(NumericBaseObserver observer) {
+        observers.add(observer);
+    }
+
+    public void unsubscribe(NumericBaseObserver observer) {
+        observers.remove(observer);
+    }
+
+    public void updateState(int value) {
+        for (NumericBaseObserver observer : observers) {
+            observer.updateState(value);
+        }
+    }
+}
