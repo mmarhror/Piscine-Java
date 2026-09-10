1c1de0d1e351fd6ec3ff344e26ea78ff6d8259c9
mmarhror <mohammedmarhror@gmail.com>
2026-09-10 10:42:21 +0100

    Quick push

--- Factory/DriverFactory.java
+++ Factory/DriverFactory.java
@@ -1,5 +1,15 @@
+
 class DriverFactory {
-   Driver getDriver(String type) {
-    
-   }
+
+    Driver getDriver(String type) {
+        return switch (type) {
+            case "CarDriver" ->
+                new CarDriver();
+            case "Plane" ->
+                new PlaneDriver();
+            default ->
+                null;
+        };
+    }
+
 }
